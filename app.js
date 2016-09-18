15110cdae04d54f0a8bad568e421f64b4a891204
kallaugher <alicekallaugher@gmail.com>
2016-09-18 14:39:43 -0400

    finish challenge

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ $(document).ready(function(){
     })
   })
 });
+
+$('form.cat-form').submit(function(event) {
+  event.preventDefault();
+  var catName = $('#cat-name').val()
+  var catTemperament = $('#cat-temperament').val()
+  var catBreed = $('#cat-breed').val()
+  var catWeight = $('#cat-weight').val()
+  var values = {cat: {name: catName, temperament: catTemperament, breed: catBreed, weight: catWeight}}
+
+  var posting = $.post('http://localhost:3000/api/v1/cats', values);
+
+  posting.done(function(data) {
+      $(".cats-index").append(`<li>${data.name} is ${data.temperament}</li>`)
+  })
+});
